ad31043b09c95d4203de927221dca90d08c70494
ielixirz <ielixirz@gmail.com>
2020-10-12 15:49:05 +0700

    adding: local library and file scan

---
+++ src/renderer/pages/LocalLibrary/index.tsx
@@ -0,0 +1,78 @@
+import clsx from "clsx";
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "../../components/Header";
+import { useMst } from "../../models";
+import * as mm from "music-metadata";
+import Playlist from "../../components/LocalLibrary/Playlist";
+const fs = require("fs");
+const path = require("path");
+
+const parseAsAudio = async (files: any[]) => {
+  var audioData: any[] = [];
+
+  for (const file of files) {
+    const metadata: mm.IAudioMetadata = await mm.parseFile(file, {
+      skipCovers: true,
+    });
+    audioData.push({
+      metadata,
+      path: file,
+    });
+  }
+  return audioData;
+};
+
+const scanFiles = (filepath: string, filelist: any[]) => {
+  const files = fs.readdirSync(filepath);
+  filelist = filelist || [];
+  files.forEach((file: any) => {
+    if (fs.statSync(path.join(filepath, file)).isDirectory()) {
+      filelist = scanFiles(path.join(filepath, file), filelist);
+    } else {
+      if (
+        file.endsWith(".mp3") ||
+        file.endsWith(".m4a") ||
+        file.endsWith(".webm") ||
+        file.endsWith(".wav") ||
+        file.endsWith(".aac") ||
+        file.endsWith(".ogg") ||
+        file.endsWith(".opus")
+      ) {
+        filelist.push(path.join(filepath, file));
+      }
+    }
+  });
+  return filelist;
+};
+
+function Home() {
+  const store = useMst();
+
+  useEffect(() => {
+    const { local } = store.player;
+    const loadedFiles = scanFiles(local.path, []);
+    parseAsAudio(loadedFiles)
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e));
+  });
+
+  const dark = store.player.theme === "dark";
+
+  return (
+    <div className={clsx(dark && "bg-dark", "min-h-screen")}>
+      <Header title="Library" dark={dark} />
+      <p className={clsx(dark && "text-white", "text-center mt-6")}>
+        <Playlist />
+      </p>
+    </div>
+  );
+}
+
+export default function () {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
